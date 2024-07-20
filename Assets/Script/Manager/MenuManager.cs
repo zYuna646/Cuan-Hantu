94e42fcbee35e0f254fbb4c9346cf2818f549b96
zYuna646 <anggarangga646@gmail.com>
2024-07-20 12:48:26 +0800

    fix

--- Assets/Script/Manager/MenuManager.cs
+++ Assets/Script/Manager/MenuManager.cs
@@ -8,13 +8,13 @@ public class MenuManager : MonoBehaviour
     public GameObject mainMenu;
     void Start()
     {
-        MainMenu();   
+        MainMenu();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void MainMenu()
@@ -22,41 +22,41 @@ public class MenuManager : MonoBehaviour
         mainMenu.SetActive(false);
     }
 
-public void StartNewGame(int slot, string name)
-{
-    SaveManager.Instance.DeleteGame(slot);
-    
-    int coins = 0;
-    int reputation = 0;
-    int level = 1;
-    int fare = 25;
-    
-    int[][] ghost = new int[][]
+    public void StartNewGame(int slot, string name)
     {
+        SaveManager.Instance.DeleteGame(slot);
+
+        int coins = 0;
+        int reputation = 0;
+        int level = 1;
+        int fare = 25;
+
+        int[][] ghost = new int[][]
+        {
         new int[] { 0, 0 },
-    };
-    
-    int[] ghostItem = new int[0]; 
-    
-    int[][] rooms = new int[][]
-    {
+        };
+
+        int[] ghostItem = new int[0];
+
+        int[][] rooms = new int[][]
+        {
         new int[] { 0, 1 },
         new int[] { 0, -1 }
-    };
-    int[][] ghostId = new int[][]
-    {
+        };
+        int[][] ghostId = new int[][]
+        {
         new int[] { 0, 0 },
-    };
-    int[][] roomsId = new int[][]
-    {
+        };
+        int[][] roomsId = new int[][]
+        {
         new int[] { 0, 0 },
-    };
-    
-    int[] roomItem = new int[0]; 
-    
-    SaveManager.Instance.SaveGame(slot, coins, reputation, ghost, ghostId,level, ghostItem, rooms,roomsId, roomItem, name, fare);
-    GameSceneManager.Instance.LoadNextScene();
-}
+        };
+
+        int[] roomItem = new int[0];
+
+        SaveManager.Instance.SaveGame(slot, coins, reputation, ghost, ghostId, level, ghostItem, rooms, roomsId, roomItem, name, fare);
+        GameSceneManager.Instance.LoadNextScene();
+    }
 
 
     public void ContinueGame(int slot)
@@ -65,7 +65,7 @@ public void StartNewGame(int slot, string name)
         string name;
         int[][] ghost, rooms, ghostId, roomsId;
         int[] ghostItem, roomItem;
-        SaveManager.Instance.LoadGame(slot, out coins, out reputation, out ghost, out ghostId,out level, out ghostItem, out rooms,out roomsId, out roomItem, out name, out fare);
+        SaveManager.Instance.LoadGame(slot, out coins, out reputation, out ghost, out ghostId, out level, out ghostItem, out rooms, out roomsId, out roomItem, out name, out fare);
         PlayerManager.Instance.SetStats(coins, reputation, level, fare, ghost, ghostId, ghostItem, rooms, roomsId, roomItem, name);
         GameSceneManager.Instance.LoadNextScene();
     }
